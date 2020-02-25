4138adefb99aab4667a3b65696aca86f9de90266
王鸿铭 <Whm990308>
2020-02-25 17:11:48 +0800

    Beautify webpage (on behalf of 王鸿铭)

--- src/benchmark.py
+++ src/benchmark.py
@@ -1,5 +1,5 @@
 
-import math
+#import math
 import random
 import string
 import statistics
@@ -26,21 +26,27 @@ def test_algorithm(data: list, round: int, algorithm):
     """ perform a round of performance test for the given algorithm """
     numbers = []
     for r in range(round):
+        gc.collect()
+        gc.freeze()
+        gc.disable()
         t0 = time.time()
         algorithm(data)
+        gc.enable()
+        gc.unfreeze()
         t1 = time.time()
         numbers.append((t1 - t0) * 1000)
 
-    return [statistics.median(numbers), statistics.mean(numbers), statistics.stdev(numbers)]
+    return [statistics.mean(numbers)]
+    #return [statistics.median(numbers), statistics.mean(numbers), statistics.stdev(numbers)]
 
 def test_all_algorithms(data_size: int, data_count: int, round: int, algorithms: list):
     data = generate_data(data_size, data_count)
     for a in algorithms:
         times = test_algorithm(data, round, a)
-        print(f"{data_size},{a.__name__},{times[0]},{times[1]},{times[2]}")
+        print(f"{data_size},{a.__name__},{times}")
 
 
 if __name__ == "__main__":
-    gc.disable()
-    algorithms = [ZeroMerkleTree, BitcoinMerkleTree, LibraMerkleTree]
-    test_all_algorithms(1024, (1 << 10) + 1, 100, algorithms)
+    #gc.disable()
+    algorithms = [BitcoinMerkleTree, LibraMerkleTree, ZeroMerkleTree]
+    test_all_algorithms(128, (1 << 10) + 1, 400, algorithms)
